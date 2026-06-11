21986810e4fc3e625766e24a9e8d422e6040b9bd
MrRedPhysio <mrred.alex@yahoo.co.uk>
2026-06-11 11:12:52 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,29 @@
 const CACHE_NAME = 'mr-ahmed-virtual-patient-v1';
-const ASSETS_TO_CACHE = [
+const APP_SHELL = [
   './',
   './index.html',
   './manifest.json',
-  './assets/mr-ahmed-portrait.png',
-  './assets/icon-192.png',
-  './assets/icon-512.png',
-  './assets/icon-maskable-512.png'
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './assets/mr-ahmed-portrait.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).catch(() => null)
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
   event.respondWith(
     caches.match(event.request).then(cached => {
       return cached || fetch(event.request).then(response => {
